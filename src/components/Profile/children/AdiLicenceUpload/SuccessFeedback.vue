50ffa78994d44f1c01a7a88a92332fd701a78001
connor <connor@codegood.co>
2018-05-13 22:17:26 +0100

    build

--- src/components/Profile/children/AdiLicenceUpload/SuccessFeedback.vue
+++ src/components/Profile/children/AdiLicenceUpload/SuccessFeedback.vue
@@ -20,7 +20,7 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 
 @Component({})
-export default class AdiLicenceUploadSuccessMessage extends Vue {}
+export default class AdiLicenceUploadSuccessMessage extends Vue { }
 </script>
 
 
